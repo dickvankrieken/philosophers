1973311b069c9301937f57ee5e01c2a33cd5e880
dirkvankrieken <45898176+dirkvankrieken@users.noreply.github.com>
2022-05-23 14:27:10 +0200

    remove set_start_time function

--- philo/incl/time.h
+++ philo/incl/time.h
@@ -6,7 +6,7 @@
 /*   By: dvan-kri <dvan-kri@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/12 14:08:41 by dvan-kri      #+#    #+#                 */
-/*   Updated: 2022/05/17 18:13:03 by dvan-kri      ########   odam.nl         */
+/*   Updated: 2022/05/18 10:48:52 by dvan-kri      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 # include "philosophers.h"
 
 void	usleep_more_accurate(t_data *data, size_t time);
-void	set_start_time(t_data *data);
 size_t	time_stamp(void);
 size_t	time_passed(size_t start);
 
