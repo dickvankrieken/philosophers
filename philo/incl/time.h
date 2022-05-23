9e292df82fb1a805e642ea6cadb636358412522b
dirkvankrieken <45898176+dirkvankrieken@users.noreply.github.com>
2022-05-23 15:36:07 +0200

    norme

--- philo/incl/time.h
+++ philo/incl/time.h
@@ -6,7 +6,7 @@
 /*   By: dvan-kri <dvan-kri@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/12 14:08:41 by dvan-kri      #+#    #+#                 */
-/*   Updated: 2022/05/23 15:21:09 by dvan-kri      ########   odam.nl         */
+/*   Updated: 2022/05/23 15:35:42 by dvan-kri      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "philosophers.h"
 
-void	usleep_more_accurate(t_data *data, long long time);
+void		usleep_more_accurate(t_data *data, long long time);
 long long	time_stamp(void);
 long long	time_passed(long long start);
 
